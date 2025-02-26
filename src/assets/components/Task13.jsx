c9c273c7d21948f82da131a8a09643ee4894e028
gauthmz <gauthambabu12@gmail.com>
2025-02-26 14:37:28 +0530

    Add Navbar and enhance Task13 component layout and content

--- src/assets/components/Task13.jsx
+++ src/assets/components/Task13.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Task13 = () => {
   return (
-    <div>Task13</div>
+    <div>
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <br />
+            <h1>Task 13</h1>
+            <br />
+            <p>Binary Substitution Cipher  (b=0 ,B=1)</p>
+            <div class="nes-container is-rounded">
+              <p>BbBBbBBB BbBBbBBb BbBBbBbB BbBBbBBb bbbbbbBb BbBBbBBb BbBBbBbB BbBBBBBb BbBBBBBb BbBBBBBb BbBBbBBb BbBBbBbB BbBBbBBb bbbbbbBb BbBBbBBb BbBBBbBb BbBBbBbB BbBBbBBb bbbbbbBb BbBBBBBb BbBBbBBb
+              </p>
+            </div>
+            <br />
+            <br />
+            <a type="button" class="nes-btn is-success" href=''>Submission</a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
