06b33b319628579a9177196090e00c4b7b27c982
gauthmz <gauthambabu12@gmail.com>
2025-02-26 18:45:38 +0530

    Enhance Task2 component with background image, updated styles, and refined content

--- src/assets/components/Task2.jsx
+++ src/assets/components/Task2.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import Navbar from "./Navbar";
+
 const Task2 = () => {
+  const backgroundImageUrl = '/tron.jpg';
+
   return (
-    <div>
+    <div style={{ backgroundImage: `url(${backgroundImageUrl})`, backgroundSize: 'cover', backgroundPosition: 'center 30px', minHeight: '100vh' }}>
       <Navbar />
       <div className="container">
         <div className="row">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <br />
-            <h1>Task 2</h1>
+            <h1 style={{ color: 'cyan' }}>Task 2</h1>
             <br />
-            <p class="nes-balloon from-left nes-pointer">
-              Sometimes, what you seek lies beneath the surface. Inspect closely; the layers of truth might surprise you.</p>
+            <p className="nes-balloon from-left nes-pointer">
+              Inspect closely; the layers of truth might surprise you.
+            </p>
             <br />
-            <a type="button" class="nes-btn is-primary" href="https://drive.google.com/file/d/1IQMUh6-FNEje5WoP7RlSU7Ql7s7e_QcW/view?usp=drive_link">Download File</a>
+            <a type="button" className="nes-btn is-primary" href="https://drive.google.com/file/d/1IQMUh6-FNEje5WoP7RlSU7Ql7s7e_QcW/view?usp=drive_link">Download File</a>
             <br />
             <br />
-            <br />
-            <a type="button" class="nes-btn is-success" href=''>Submission</a>
+            <a type="button" className="nes-btn is-success" href=''>Submission</a>
           </div>
         </div>
       </div>
