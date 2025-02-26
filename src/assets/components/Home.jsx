2aaa26c225e1b91d9870c3fec1bd1c2aa7033ea9
ZENZEPHY <adithkvxid@gmail.com>
2025-02-26 17:38:17 +0530

    Update Home and Navbar styles

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Navbar from "./Navbar";
-
+import "./Home.css";
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <div className="background">
+        <video autoPlay loop muted className="background-video">
+          <source src="/Mwone.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="overlay"></div>
+      </div>
     </div>
   );
 };
