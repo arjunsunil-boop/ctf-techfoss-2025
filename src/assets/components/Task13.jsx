6f93c9c7c38b38b00ef167f73f886bc6cb3613b6
gauthmz <gauthambabu12@gmail.com>
2025-02-26 19:29:33 +0530

    Add background image and improve text visibility in Task13 component

--- src/assets/components/Task13.jsx
+++ src/assets/components/Task13.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import Navbar from './Navbar'
 
 const Task13 = () => {
+  const backgroundImageUrl = '/binarycipher.jpg';
+
   return (
-    <div>
+    <div style={{ backgroundImage: `url(${backgroundImageUrl})`, backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh' }}>
       <Navbar />
       <div className="container">
         <div className="row">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <br />
-            <h1>Task 13</h1>
+            <h1 >Task 13</h1>
             <br />
-            <p>Binary Substitution Cipher  (b=0 ,B=1)</p>
-            <div class="nes-container is-rounded">
-              <p>bBbbbBBb bBbbBBbb bBbbbbbB bBbbbBBB bBbBBBBB bBbbbbBB bBbbbbbB bBbBbbbb bBbBbBbb bBbBbBbB bBbBbbBb bBbbbBbB bBbbbBbb bBbBBBBB bBbbbBBB bBbbbbbB bBbbBBbB bBbbbBbB bBbBBBBB bBbbBBBB bBbbBBBb</p>
+            <p >Binary Substitution Cipher  (b=0,B=1)</p>
+            <div class="nes-container is-rounded" style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}>
+              <p style={{ color: 'white' }}>bBbbbBBb bBbbBBbb bBbbbbbB bBbbbBBB bBbBBBBB bBbbbbBB bBbbbbbB bBbBbbbb bBbBbBbb bBbBbBbB bBbBbbBb bBbbbBbB bBbbbBbb bBbBBBBB bBbbbBBB bBbbbbbB bBbbBBbB bBbbbBbB bBbBBBBB bBbbBBBB bBbbBBBb</p>
             </div>
             <br />
             <br />
