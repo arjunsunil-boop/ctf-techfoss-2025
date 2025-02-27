4cd50037a742eb9ed8d0534747fb04e013d20700
Alwin-Sajan <alwinsajan225@gmail.com>
2025-02-27 07:35:25 +0530

    created task 4

--- src/assets/components/Task9.jsx
+++ src/assets/components/Task9.jsx
@@ -8,41 +8,41 @@ const Task9 = () => {
   const navigate = useNavigate(); // Correct way to handle navigation
 
   const verify = () => {
-    if (ans === "4") {
-      window.open("https://forms.google.com/example-form", "_blank");
-      navigate("/t10pqrst");  // Use navigate instead of redirect
-    }
+
+    window.open("https://forms.google.com/example-form", "_blank");
+    navigate("/t10pqrst");  // Use navigate instead of redirect
+
   };
 
   return (
-    <div className='h-auto w-full'>
+    <div className='h-full w-full' id='task9bg'>
       <Navbar />
-      <div className='bg-[#4fb15c] grid justify-center h-[100%] py-8 px-16'>
-        <h2 className=' text-white color text-lg'>Task 9</h2>
+      <div className=' grid justify-center h-full py-8 px-16'>
+        <h2 className=' text-white color text-md'>Task 9</h2>
 
-        <div className=' p-4 items-stretch '>
-          <p className='text-yellow-300 '>
+        <div className='h-full p-3 items-stretch '>
+          <p className='text-blue-500 '>
             I took a secret number X and performed these operations:
 
           </p>
-          <p className='text-pink-800' >
-            XORed it with 3<br/>
-            ANDed it with 7<br/>
-            ORed it with 12<br/>
-            The final result is 15.<br/>
+          <p className='text-green-300 ' >
+            XORed it with 3<br />
+            ANDed it with 7<br />
+            ORed it with 12<br />
+            The final result is 15.<br />
             X {'>'} 0
           </p>
-          <p>What was my original number X?</p>
+          <p className='text-yellow-300'>What was my original number <strong className='text-red-500'>X</strong>?</p>
 
           <div className='flex items-center gap-4 '>
-            <label htmlFor="answer" className='text-white'>Submit Here</label>
+            {/* <label htmlFor="answer" className='text-white'>Submit Here</label>
             <input
               type="text"
               id="answer"
               value={ans}
               onChange={(e) => setAns(e.target.value)}  // Update state on input change
               className='bg-blue-500 text-white p-2'
-            />
+            /> */}
             <button
               type="button"
               className='bg-red-500 p-2 text-yellow-300'
