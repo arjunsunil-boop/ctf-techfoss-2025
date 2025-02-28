8012e6a11b08a1e2073d1ccb4903ffdce9436385
Alwin-Sajan <alwinsajan225@gmail.com>
2025-02-28 13:25:14 +0530

    fix the task 2 routing

--- src/assets/components/Task2.jsx
+++ src/assets/components/Task2.jsx
@@ -20,7 +20,7 @@ const Task2 = () => {
             <a type="button" className="nes-btn is-primary" href="https://drive.google.com/file/d/1ob9wSaRXdeChmbaAKVJ48Fr4367ihRxF/view?usp=sharing">Download File</a>
             <br />
             <br />
-            <a type="button" className="nes-btn is-success" href=''>Submission</a>
+            <a type="button" className="nes-btn is-success" href='' target='/blank'>Submission</a>
           </div>
         </div>
       </div>
