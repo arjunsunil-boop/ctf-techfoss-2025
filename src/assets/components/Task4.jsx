d9146022563234296b995425f980c2e88890bd63
Alwin-Sajan <alwinsajan225@gmail.com>
2025-02-28 12:44:05 +0530

    completed task 4

--- src/assets/components/Task4.jsx
+++ src/assets/components/Task4.jsx
@@ -4,22 +4,38 @@ import './tailcss.css';
 import { useNavigate } from 'react-router-dom';
 
 const Task4 = () => {
-  const navigate = useNavigate();
   const [ans, setAns] = useState('');
+  const [message, setMessage] = useState('');
+  const [showNext, setShowNext] = useState(false);
+  const navigate = useNavigate();
+
+
+  var FLAG = 'techfoss<CLEVELAND>';
 
   const verify = () => {
-    if (ans === "//www.scrambled.com") { // Check if the answer is correct
-      navigate('/t5qrst'); // Navigate only when answer is correct
+    if (ans === '//www.scrambled.com') {
+      setMessage(<div className="bg-green-200 text-green-700 p-2 mt-2">Correct Password!</div>);
+      setShowNext(true);
     } else {
-      alert("Wrong answer! Try again.");
       setAns('');
+      setMessage(<div className="bg-red-200 text-red-700 p-2 mt-2">Wrong Password!</div>);
+      setTimeout(() => {
+        setMessage('');
+      }, 2000);
+      setShowNext(false);
     }
   };
+
+  const handleNext = () => {
+    navigate('/t5qrst');
+  };
+
+
 
   return (
     <div id='task4bg'>
       <Navbar />
-      
+
       <div className='container p-12'>
         <h2 className='text-md text-white'>Task 4</h2>
         <div className='mt-16 w-3/4'>
@@ -35,7 +51,7 @@ const Task4 = () => {
             type="text"
             id="answer"
             value={ans}
-            onChange={(e) => setAns(e.target.value)}  
+            onChange={(e) => setAns(e.target.value)}
             className='bg-blue-500 text-white p-2'
           />
           <button
@@ -45,7 +61,23 @@ const Task4 = () => {
           >
             Submit
           </button>
+          {message}
         </div>
+        {showNext && (
+          <div className='py-8 px-2 flex gap-4'>
+
+            <div className="bg-green-100 text-green-700 p-2">flag: {FLAG}</div>
+            <button
+              type="button"
+              className='bg-purple-500 p-2 text-yellow-300'
+              onClick={handleNext}  // Call verify function correctly
+            >
+              Next Task
+            </button>
+
+
+          </div>
+        )}
       </div>
     </div>
   );
